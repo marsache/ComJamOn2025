211f7c97156785ea454f2e517954aa65f0a5eef8
María Sachez <marsache@ucm.es>
2025-03-15 12:31:59 +0100

    DALe movimiento

---
+++ Jara la Charramanguera/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource music;
+    public bool startPlaying;
+    public BeatScroller beatScroller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                beatScroller.hasStarted = true;
+
+                music.Play();
+            }
+        }
+    }
+}
